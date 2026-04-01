7831ea1d710d1a0331b128320739175b60cef929
ksinghvi9 <singhvikavy@gmail.com>
2026-04-01 18:12:08 +0530

    good start

---
+++ JS/home.js
@@ -0,0 +1,10 @@
+let happyBtn = document.getElementById("happy");
+let sadBtn = document.getElementById("sad");
+
+happyBtn.addEventListener("click", function () {
+    console.log("Happy mood selected");
+});
+
+sadBtn.addEventListener("click", function () {
+    console.log("Sad mood selected");
+});
